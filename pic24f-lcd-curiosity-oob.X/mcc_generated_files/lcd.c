b6f9857d0998b4189828770645b6d4ecedb18f76
Manjunatha Mahesh <Manjunatha.Mahesh@microchip.com>
2020-09-18 20:23:21 +0530

    Removed hardware.txt file Changed the alternate display mode delay to  2.5s

--- pic24f-lcd-curiosity-oob.X/mcc_generated_files/lcd.c
+++ pic24f-lcd-curiosity-oob.X/mcc_generated_files/lcd.c
@@ -108,7 +108,7 @@ void LCD_ModeSet(enum LCD_MODE mode)
                     LCDACTRLbits.BLINKMODE = 0x0000;//Disable Blink Mode
                     LCDASTATbits.DMSEL = 0x0003;//Continuous switch over between primary and secondary memory
                     LCDACTRLbits.SMFCS = 0x0001;
-                    LCDFC0 = 0x0b59;
+                    LCDFC0 = 0x0975;
                     LCDACTRLbits.ELCDEN = 1;//Enable LCD enhanced Mode
                     break;
 
--- pic24f-lcd-curiosity-oob.X/mcc_generated_files/mcc.h
+++ pic24f-lcd-curiosity-oob.X/mcc_generated_files/mcc.h
@@ -51,17 +51,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "uart1.h"
-#include "lcd.h"
 #include "tmr3.h"
-#include "spi1.h"
-#include "rtcc.h"
+#include "uart1.h"
+#include "adc1.h"
+#include "lcd.h"
 #include "mccp4_compare.h"
-#include "mccp3_compare.h"
-#include "mccp2_compare.h"
 #include "interrupt_manager.h"
 #include "traps.h"
-#include "adc1.h"
+#include "mccp3_compare.h"
+#include "rtcc.h"
+#include "mccp2_compare.h"
+#include "spi1.h"
 
 #warning "This file will be removed in future MCC releases. Use system.h instead."
 
